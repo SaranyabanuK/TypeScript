f84e892a6d5596006de6bdd8da6823a6f6176e5b
SaranyabanuK <banus4274@gmail.com>
2025-07-21 02:24:31 -0700

    Add types examples with string, number, boolean types

---
+++ Work-Space/2.Types/example3.js
@@ -0,0 +1,7 @@
+// types-example.ts
+var userName = "Saranya";
+var userAge = 22;
+var isLearningTS = true;
+console.log("Name:", userName);
+console.log("Age:", userAge);
+console.log("Learning TypeScript:", isLearningTS);
---
+++ Work-Space/2.Types/example3.ts
@@ -0,0 +1,9 @@
+// types-example
+
+let userName: string = "Saranya";
+let userAge: number = 22;
+let isLearningTS: boolean = true;
+
+console.log("Name:", userName);
+console.log("Age:", userAge);
+console.log("Learning TypeScript:", isLearningTS);
